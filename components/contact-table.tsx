46eb39a681c7eb24b431b2e9b74a6ee6e217f4d2
rianmz-genz <adrianji1945@gmail.com>
2024-02-19 11:31:25 +0700

    crud contacts

--- components/contact-table.tsx
+++ components/contact-table.tsx
@@ -1,14 +1,23 @@
 import { getContacts } from '@/lib/data';
 import { formatDate } from '@/lib/utils';
-const ContactTable = async () => {
-  const contacts = await getContacts();
+import { EditButton, DeleteButton } from '@/components/buttons';
+
+const ContactTable = async ({
+  query,
+  currentPage,
+}: {
+  query: string;
+  currentPage: number;
+}) => {
+  const contacts = await getContacts(query, currentPage);
+
   return (
     <table className='w-full text-sm text-left text-gray-500'>
       <thead className='text-sm text-gray-700 uppercase bg-gray-50'>
         <tr>
           <th className='py-3 px-6'>#</th>
           <th className='py-3 px-6'>Name</th>
-          <th className='py-3 px-6'>Phone</th>
+          <th className='py-3 px-6'>Phone Number</th>
           <th className='py-3 px-6'>Created At</th>
           <th className='py-3 px-6 text-center'>Actions</th>
         </tr>
@@ -25,7 +34,10 @@ const ContactTable = async () => {
             <td className='py-3 px-6'>
               {formatDate(contact.createdAt.toString())}
             </td>
-            <td className='py-3 px-6'></td>
+            <td className='flex justify-center gap-1 py-3'>
+              <EditButton id={contact.id} />
+              <DeleteButton id={contact.id} />
+            </td>
           </tr>
         ))}
       </tbody>
